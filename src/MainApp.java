acef23d500eb74a671a00fc28ad60bd6361dc700
Rav270593 <44171972+Rav270593@users.noreply.github.com>
2018-10-17 21:43:56 +0200

    Class of magic number symbols for comparsion

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,31 @@
-import java.io.File;
 import java.io.FileInputStream;
+
+class MagicNum {
+	String hex1;
+	String hex2;
+	String hex3;
+	String hex4;
+	String hex5;
+	
+	public MagicNum() {
+	}
+	/*
+	public MagicNum(String hex1,String hex2,String hex3,String hex4) {
+		this.hex1 = hex1;
+		this.hex2 = hex2;
+		this.hex3 = hex3;
+		this.hex4 = hex4;
+	}
+	
+	public MagicNum(String hex1,String hex2,String hex3,String hex4,String hex5) {
+		this.hex1 = hex1;
+		this.hex2 = hex2;
+		this.hex3 = hex3;
+		this.hex4 = hex4;
+		this.hex5 = hex5;
+	}
+	*/
+}
 
 public class MainApp {
 	public static void main(String[] argv) throws Exception {
@@ -8,7 +34,12 @@ public class MainApp {
 		int len;
 	    byte data[] = new byte[16];
 	    len = fileIn.read(data);
-		for (int j = 0; j < 5; j++)
-		    System.out.printf("%02X ", data[j]);		      	
+		for (int j = 0; j < 5; j++) 
+		    System.out.printf("%02X ", data[j]);
+		
+		MagicNum fileX = new MagicNum();
+		//fileX.hex1 = String.valueOf("%02X ",data[0]);
+		fileX.hex1 =String.format("%02x", Byte.parseByte(data[0]));
+		System.out.printf("%02X ",fileX.hex1); 
 	}
 }
